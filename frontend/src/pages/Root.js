01c0fd17f115b271a1d70d0e8f77c38853394f97
KimKyuHoi <k546kh@gmail.com>
2023-10-31 02:16:36 +0900

    [Feat] : 로그인 공부하기 완료

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,30 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
   // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' })
+    }, 1 * 60 * 60 * 1000);
+  }, [token, submit]);
 
   return (
     <>
